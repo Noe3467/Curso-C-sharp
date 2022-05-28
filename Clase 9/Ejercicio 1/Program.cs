710a92c1c2b2713425db1fb4dcddb5f056267af9
Noelia Romina Ruiz <estefitrinidad@gmail.com>
2022-05-28 06:35:21 -0300

    Clase 9

---
+++ Clase 9/Ejercicio 1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+/*
+1)      Pedir al usuario la longitud de un vector
+2)      Crear el vector del tamaño ingresado.
+3)      Llenar el mismo con datos aleatorios
+4)      Mostrar el vector por pantalla
+5)      Invertir el vector, de manera que el primer elemento quede al último y el útimo en el primero; el segundo en anteúltimo, el anteúltimo en el segundo y así sucesivamente. En otra palabras si el vector es de 5 posiciones y el usuario ingresó: 10, 20, 30, 40, 50, una vez invertido, el vector debe quedar así: 50, 40, 30, 20, 10.  
+Se debe usar solo lo visto en clase hasta ahora y no los métodos que trae C# para estas cuestiones. Tampoco se debe crear un vector nuevo o auxiliar para realizar el ejercicio. 
+6)      Mostrar el vector nuevamente. 
+*/
+
+Random aleatorio = new Random();
+Console.WriteLine("ingresá la cantidad de números aleatorios"); //paso1
+int cantNumAleat = int.Parse(Console.ReadLine()); 
+int[] aleatorios = new int[cantNumAleat]; //paso2
+
+for (int i = 0;  i< aleatorios.Length; i++)
+{
+    aleatorios[i] = aleatorio.Next(0,100); //paso3
+}
+foreach (int numero in aleatorios)
+{
+    Console.Write("[" + numero + "]");
+}
+Console.WriteLine();
+for (int i = aleatorios.Length-1; i >=0 ; i--)
+{
+    Console.Write("[" + aleatorios[i] + "]");
+}
+
+
+
+
